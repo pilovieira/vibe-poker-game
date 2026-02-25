83b16ff4ca5890dc43c7a6ec2afc59ce029b00b6
Luiz Paulo Vieira <pilovieira@gmail.com>
2026-02-25 12:06:50 -0300

    move project to new repo

---
+++ public/database.js
@@ -0,0 +1,100 @@
+const rankingsData = {
+    "2026": [
+        { "name": "Pedro", "wins": 1, "dates": ["19/01/2026"] },
+        { "name": "Galende", "wins": 1, "dates": ["02/02/2026"] },
+        { "name": "Bille", "wins": 1, "dates": ["23/02/2026"] }
+    ],
+    "2025": [
+        { "name": "Paulinho", "wins": 4 },
+        { "name": "Pilo", "wins": 3 },
+        { "name": "Maiko", "wins": 3 },
+        { "name": "Ramao", "wins": 3 },
+        { "name": "Galende", "wins": 3 },
+        { "name": "Afonso", "wins": 2 },
+        { "name": "Leo", "wins": 1 },
+        { "name": "Alex", "wins": 1 },
+        { "name": "Bille", "wins": 1 }
+    ],
+    "2024": [
+        { "name": "Paulinho", "wins": 3 },
+        { "name": "Ramao", "wins": 3 },
+        { "name": "Pc", "wins": 2 },
+        { "name": "Zé", "wins": 2 },
+        { "name": "Leo", "wins": 1 },
+        { "name": "Afonso", "wins": 1 },
+        { "name": "Galende", "wins": 1 },
+        { "name": "Jafer", "wins": 1 },
+        { "name": "Pilo", "wins": 1 },
+        { "name": "Maykon", "wins": 1 }
+    ],
+    "2023": [
+        { "name": "Pc", "wins": 3 },
+        { "name": "Pilo", "wins": 3 },
+        { "name": "Afonso", "wins": 2 },
+        { "name": "Galende", "wins": 2 }
+    ],
+    "2000": [
+        { "name": "Luan", "wins": 0 },
+        { "name": "Rodrigo", "wins": 0 }
+    ]
+};
+
+const hallOfFameData = [
+    {
+        "name": "Rodrigo",
+        "date": "23/02/2026",
+        "hand": "10♦️10♠️10♥️10♣️"
+    },
+    {
+        "name": "Afonso",
+        "date": "19/01/2026",
+        "hand": "4♦️4♠️4♥️4♣️"
+    },
+    {
+        "name": "Leo",
+        "date": "01/12/2025",
+        "hand": "10♦️10♠️10♥️10♣️"
+    },
+    {
+        "name": "Alex",
+        "date": "27/10/2025",
+        "hand": "A♦️A♠️A♥️A♣️"
+    },
+    {
+        "name": "Maiko",
+        "date": "13/10/2025",
+        "hand": "J♦️J♠️J♥️J♣️"
+    },
+    {
+        "name": "Alex",
+        "date": "04/08/2025",
+        "hand": "10♦️J♦️Q♦️K♦️A♦️"
+    },
+    {
+        "name": "Pilo",
+        "date": "05/02/2024",
+        "hand": "10♠️J♠️Q♠️K♠️A♠️"
+    },
+    {
+        "name": "Paulinho",
+        "date": "30/10/2023",
+        "hand": "9♦️10♦️J♦️Q♦️K♦️"
+    }
+];
+const avatarsData = {
+    "Galende": "./avatar/galende.png",
+    "Paulinho": "./avatar/paulinho.png",
+    "Pilo": "./avatar/pilo.png",
+    "Pc": "./avatar/pc.png",
+    "Zé": "./avatar/ze.png",
+    "Leo": "./avatar/leo.png",
+    "Afonso": "./avatar/afonso.png",
+    "Alex": "./avatar/alex.png",
+    "Maiko": "./avatar/maiko.png",
+    "Bille": "./avatar/bille.png",
+    "Ramao": "./avatar/ramao.png",
+    "Jafer": "./avatar/jafer.png",
+    "Maykon": "./avatar/maykon.png",
+    "Luan": "./avatar/luan.png",
+    // "Pedro": "./avatar/pedro.png"
+};
