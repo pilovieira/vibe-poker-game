1e95bcfbf2288a75532b96d3cfde1d2fde9429bd
Luiz Paulo Vieira <pilovieira@gmail.com>
2026-04-14 08:22:40 -0300

    update ranking

--- public/database.js
+++ public/database.js
@@ -1,10 +1,10 @@
 const rankingsData = {
     "2026": [
+        { "name": "Alex", "wins": 2, "dates": ["23/03/2026", "13/04/2026"] },
         { "name": "Pedro", "wins": 1, "dates": ["19/01/2026"] },
         { "name": "Galende", "wins": 1, "dates": ["02/02/2026"] },
         { "name": "Bille", "wins": 1, "dates": ["23/02/2026"] },
-        { "name": "Ramao", "wins": 1, "dates": ["09/03/2026"] },
-        { "name": "Alex", "wins": 1, "dates": ["23/03/2026"] }
+        { "name": "Ramao", "wins": 1, "dates": ["09/03/2026"] }
     ],
     "2025": [
         { "name": "Paulinho", "wins": 4 },
@@ -43,52 +43,18 @@ const rankingsData = {
 };
 
 const hallOfFameData = [
-    {
-        "name": "Jardel",
-        "date": "09/03/2026",
-        "hand": "5♦️5♠️5♥️5♣️"
-    },
-    {
-        "name": "Rodrigo",
-        "date": "23/02/2026",
-        "hand": "10♦️10♠️10♥️10♣️"
-    },
-    {
-        "name": "Afonso",
-        "date": "19/01/2026",
-        "hand": "4♦️4♠️4♥️4♣️"
-    },
-    {
-        "name": "Leo",
-        "date": "01/12/2025",
-        "hand": "10♦️10♠️10♥️10♣️"
-    },
-    {
-        "name": "Alex",
-        "date": "27/10/2025",
-        "hand": "A♦️A♠️A♥️A♣️"
-    },
-    {
-        "name": "Maiko",
-        "date": "13/10/2025",
-        "hand": "J♦️J♠️J♥️J♣️"
-    },
-    {
-        "name": "Alex",
-        "date": "04/08/2025",
-        "hand": "10♦️J♦️Q♦️K♦️A♦️"
-    },
-    {
-        "name": "Pilo",
-        "date": "05/02/2024",
-        "hand": "10♠️J♠️Q♠️K♠️A♠️"
-    },
-    {
-        "name": "Paulinho",
-        "date": "30/10/2023",
-        "hand": "9♦️10♦️J♦️Q♦️K♦️"
-    }
+    { "name": "Afonso", "date": "13/04/2026", "hand": "A♦️A♠️A♥️A♣️" },
+    { "name": "Jardel", "date": "09/03/2026", "hand": "5♦️5♠️5♥️5♣️" },
+    { "name": "Rodrigo", "date": "23/02/2026", "hand": "10♦️10♠️10♥️10♣️" },
+    { "name": "Afonso", "date": "19/01/2026", "hand": "4♦️4♠️4♥️4♣️" },
+    { "name": "Leo", "date": "01/12/2025", "hand": "10♦️10♠️10♥️10♣️" },
+    { "name": "Alex", "date": "27/10/2025", "hand": "A♦️A♠️A♥️A♣️" },
+    { "name": "Maiko", "date": "13/10/2025", "hand": "J♦️J♠️J♥️J♣️" },
+    { "name": "Alex", "date": "04/08/2025", "hand": "10♦️J♦️Q♦️K♦️A♦️" },
+    { "name": "Pilo", "date": "05/02/2024", "hand": "10♠️J♠️Q♠️K♠️A♠️" },
+    { "name": "Paulinho", "date": "30/10/2023", "hand": "9♦️10♦️J♦️Q♦️K♦️" }
 ];
+
 const avatarsData = {
     "Galende": "./avatar/galende.png",
     "Paulinho": "./avatar/paulinho.png",
